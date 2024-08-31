1a4c8d40aaac67ef7a89fbdfaca7bb686e1d3e83
nureman <nureman_s@cmu.ac.th>
2024-08-31 21:58:29 +0700

    stop relay

--- charge_mqtt/urls.py
+++ charge_mqtt/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import check_relay_status, start_mqtt_listener, start_relay
+from .views import check_relay_status, start_mqtt_listener, start_relay, stop_relay
 
 urlpatterns = [
-    path('start-listening/', start_mqtt_listener, name='start_mqtt_listener'),
-    path('relay/<int:relayID>/start/', start_relay, name='start_relay'),
-     path('relay/status/', check_relay_status, name='check_relay_status')
+    path("start-listening/", start_mqtt_listener, name="start_mqtt_listener"),
+    path("relay/<int:relayID>/start/", start_relay, name="start_relay"),
+    path("relay/status/", check_relay_status, name="check_relay_status"),
+    path("relay/<int:relayID>/stop/", stop_relay, name="stop_relay"),
 ]
--- charge_mqtt/views.py
+++ charge_mqtt/views.py
@@ -3,7 +3,7 @@ import threading
 import paho.mqtt.client as mqtt
 import os
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt 
+from django.views.decorators.csrf import csrf_exempt
 from .serializers import RelayActivation
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
@@ -11,34 +11,39 @@ from .models import RelayActivation
 
 
 # Initialize the relay status dictionary with all relays inactive
-relay_status = {1: 'inactive', 2: 'inactive', 3: 'inactive', 4: 'inactive'}
+relay_status = {1: "inactive", 2: "inactive", 3: "inactive", 4: "inactive"}
+
 
 def on_message(client, userdata, message):
-    print(f"Received message '{str(message.payload.decode())}' on topic '{message.topic}' with QoS {message.qos}")
+    print(
+        f"Received message '{str(message.payload.decode())}' on topic '{message.topic}' with QoS {message.qos}"
+    )
+
 
 def connect_mqtt():
     client = mqtt.Client()
 
     client.username_pw_set("PutipongSailen", "Putipong.48852")
 
-    ca_cert_path = os.path.join(os.path.dirname(__file__), 'cert/emqxsl-ca.crt')
-    client.tls_set(ca_certs= ca_cert_path)
+    ca_cert_path = os.path.join(os.path.dirname(__file__), "cert/emqxsl-ca.crt")
+    client.tls_set(ca_certs=ca_cert_path)
 
-    try:   
+    try:
         client.connect("wb35b1b7.ala.asia-southeast1.emqxsl.com", 8883, 60)
         print("Connected to MQTT Broker")
-    except Exception as e: 
+    except Exception as e:
         print(f"Failed to connect MQTT Broker: {e}")
         return None
-    
+
     client.on_message = on_message
     client.loop_start()
 
     return client
 
+
 def check_emqx_connection():
     client = connect_mqtt()
-    if client:  
+    if client:
         topic = "emqx/esp32"
         message = "Django server started successfully"
         client.publish(topic, message)
@@ -46,22 +51,31 @@ def check_emqx_connection():
     else:
         print("Failed to send message due to connection issue")
 
+
 @csrf_exempt
 def start_mqtt_listener(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         client = connect_mqtt()
         if client:
-            return JsonResponse({'status': 'listening'})
+            return JsonResponse({"status": "listening"})
         else:
-            return JsonResponse({'status': 'failed', 'reason': 'Connection issue'}, status=500)
-        
-    return JsonResponse({'status': 'failed', 'reason': 'invalid request method'}, status=400)
+            return JsonResponse(
+                {"status": "failed", "reason": "Connection issue"}, status=500
+            )
+
+    return JsonResponse(
+        {"status": "failed", "reason": "invalid request method"}, status=400
+    )
+
 
 def control_relay_module(relayID, duration):
     client = connect_mqtt()
     if client:
-        if relay_status.get(relayID) == 'active':
-            return JsonResponse({'status': 'failed', 'reason': f'Relay {relayID} is already active'}, status=400)
+        if relay_status.get(relayID) == "active":
+            return JsonResponse(
+                {"status": "failed", "reason": f"Relay {relayID} is already active"},
+                status=400,
+            )
 
         topic = f"relay/{relayID}/control"
         # Send the START command with the duration included in the message payload
@@ -70,7 +84,7 @@ def control_relay_module(relayID, duration):
         print(f"Sent command '{payload}' to '{topic}' for {duration} seconds")
 
         # Update relay status to active
-        relay_status[relayID] = 'active'
+        relay_status[relayID] = "active"
 
         def stop_relay():
             client.publish(topic, json.dumps({"command": "STOP"}))
@@ -78,37 +92,108 @@ def control_relay_module(relayID, duration):
             client.disconnect()
 
             # Update relay status to inactive
-            relay_status[relayID] = 'inactive'
+            relay_status[relayID] = "inactive"
 
         timer = threading.Timer(duration, stop_relay)
         timer.start()
 
-        return JsonResponse({'status': 'success', 'relay': relayID, 'duration': duration})
+        return JsonResponse(
+            {"status": "success", "relay": relayID, "duration": duration}
+        )
     else:
-        return JsonResponse({'status': 'failed', 'reason': 'Failed to Connect to MQTT Broker'}, status=500)
+        return JsonResponse(
+            {"status": "failed", "reason": "Failed to Connect to MQTT Broker"},
+            status=500,
+        )
+
+
+def stop_relay_module(relayID):
+    client = connect_mqtt()
+    if client:
+        if relay_status.get(relayID) == "inactive":
+            return JsonResponse(
+                {"status": "failed", "reason": f"Relay {relayID} is already inactive"},
+                status=400,
+            )
+
+        topic = f"relay/{relayID}/control"
+        client.publish(topic, json.dumps({"command": "STOP"}))
+        print(f"Sent command 'STOP' to '{topic}'")
+        client.disconnect()
+
+        # Update relay status to inactive
+        relay_status[relayID] = "inactive"
+
+        return JsonResponse(
+            {"status": "success", "relay": relayID, "action": "stopped"}
+        )
+    else:
+        return JsonResponse(
+            {"status": "failed", "reason": "Failed to Connect to MQTT Broker"},
+            status=500,
+        )
+
 
 @csrf_exempt
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def start_relay(request, relayID):
     user = request.user
 
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.body)
-        duration = data.get('duration', 15)
-        
+        duration = data.get("duration", 15)
+
         # Log the relay activation
         activation = RelayActivation.objects.create(
             relay_id=relayID,
             user=user,
             duration=duration * 60,
-         
         )
 
         # Control the relay module
         return control_relay_module(relayID, duration * 60)
-    
-    return JsonResponse({'status': 'failed', 'reason': 'Invalid request method'}, status=400)
+
+    return JsonResponse(
+        {"status": "failed", "reason": "Invalid request method"}, status=400
+    )
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def stop_relay(request, relayID):
+    user = request.user
+
+    if request.method == "POST":
+        return stop_relay_module(relayID)
+
+    return JsonResponse(
+        {"status": "failed", "reason": "Invalid request method"}, status=400
+    )
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def start_relay(request, relayID):
+    user = request.user
+
+    if request.method == "POST":
+        data = json.loads(request.body)
+        duration = data.get("duration", 15)
+
+        activation = RelayActivation.objects.create(
+            relay_id=relayID,
+            user=user,
+            duration=duration * 60,
+        )
+
+        return control_relay_module(relayID, duration * 60)
+
+    return JsonResponse(
+        {"status": "failed", "reason": "Invalid request method"}, status=400
+    )
 
 
 # Function to check the status of all relays
