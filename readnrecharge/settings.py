5a7861dbf1f206802098f2e8e5ff814194f299f6
nooriman <nurfarahrohadatulaisyah@gmail.com>
2025-05-01 14:31:42 +0700

    change to db.sqlite

--- readnrecharge/settings.py
+++ readnrecharge/settings.py
@@ -93,13 +93,17 @@ WSGI_APPLICATION = "readnrecharge.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": "readnrecharge",
+    #     "USER": "root",
+    #     "PASSWORD": "readnrecharge123",
+    #     "HOST": "127.0.0.1",
+    #     "PORT": "3306",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "readnrecharge",
-        "USER": "root",
-        "PASSWORD": "readnrecharge123",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
